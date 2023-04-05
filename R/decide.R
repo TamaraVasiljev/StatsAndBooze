406e674f24df4b128fdf6f11709771868924775c
Tamara Vasiljev <t.vasiljev@campus.unimib.it>
2023-04-05 16:06:29 +0200

    add decide_happy_hour + set up testthat

---
+++ R/decide.R
@@ -0,0 +1,12 @@
+#' Decide the best day for an happy hour
+#'
+#' @param x A list of Date vectors
+#'
+#' @return The set of common Date(s)
+#' @export
+#'
+#' @examples
+#' 1+1
+decide_happy_hour <- function(x) {
+  lubridate::as_date(Reduce(lubridate::intersect , x))
+}
