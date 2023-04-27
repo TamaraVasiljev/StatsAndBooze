39051f48472121a9e174e96156de0a4c0a61931a
Valentina Zangirolami <valentina.zangirolami@gmail.com>
2023-04-27 20:24:17 +0200

    add first point of last ex

--- R/parse.R
+++ R/parse.R
@@ -7,6 +7,27 @@
 #'
 #' @examples
 #' 1+1
-parse_dates<-function(x) {
-  lapply(x, lubridate::as_date)
+#'
+sequence_dates <- function(string){
+  date_int <- lubridate::interval(string)
+  date_start <- lubridate::as_date(lubridate::int_start(date_int))
+  date_end <- lubridate::as_date(lubridate::int_end(date_int))
+  sequence_interval <- seq(date_start,date_end,by = "day")
+  return(sequence_interval)
+}
+
+kind_of_dates <- function(obs){
+  new_obs <- c()
+  for(i in 1:length(obs)){
+    if(grepl('/', obs[i])){
+      new_obs <- c(new_obs, sequence_dates(obs[i]))
+    } else {
+      new_obs <- c(new_obs, lubridate::as_date(obs[i]))
+    }
+  }
+  return(new_obs)
+}
+
+parse_dates <- function(x) {
+  lapply(sapply(x, kind_of_dates), lubridate::as_date)
 }
