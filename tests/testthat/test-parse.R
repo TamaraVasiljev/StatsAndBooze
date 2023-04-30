55a3341f9315749e9df1cb4da4f71a71a707083a
Valentina Zangirolami <valentina.zangirolami@gmail.com>
2023-04-30 18:47:58 +0200

    update test

--- tests/testthat/test-parse.R
+++ tests/testthat/test-parse.R
@@ -1,5 +1,5 @@
 test_that("basic parsing works", {
-  my_string_dates<-list(andrea=c("2023-05-01","2023-05-02 / 2023-05-04"))
-  my_output<-list(andrea=c(lubridate::as_date("2023-05-01"),lubridate::as_date("2023-05-02"),lubridate::as_date("2023-05-03"),lubridate::as_date("2023-05-04")))
+  my_string_dates<-list(andrea=c("2023-05-01","2023-05-02 / 2023-05-04"), federico=c("wednesday","thursday"))
+  my_output<-list(andrea=c(lubridate::as_date("2023-05-01"),lubridate::as_date("2023-05-02"),lubridate::as_date("2023-05-03"),lubridate::as_date("2023-05-04")), federico=c(lubridate::as_date("2023-05-03"), lubridate::as_date("2023-05-04")))
   expect_equal(parse_dates(my_string_dates), my_output)
 })
