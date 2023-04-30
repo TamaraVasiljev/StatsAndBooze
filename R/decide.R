adac2c841c63354e70d09143ff365147af579077
Valentina Zangirolami <valentina.zangirolami@gmail.com>
2023-04-30 19:02:41 +0200

    final update

--- R/decide.R
+++ R/decide.R
@@ -6,9 +6,12 @@
 #' @export
 #'
 #' @examples
-#' 1+1
-#' 2+3
-#' 3+3
+#' beer_dates_string <- list( andrea = c("thursday", "friday"),
+#'  federico = "2023-05-04",
+#'  chiara = c("2023-05-03 / 2023-05-05", "2023-05-10"))
+#'
+#' dates <- parse_dates(beer_dates_string)
+#' decide_happy_hour(dates)
 decide_happy_hour <- function(x) {
   lubridate::as_date(Reduce(lubridate::intersect , x))
 }
--- R/parse.R
+++ R/parse.R
@@ -13,45 +13,45 @@
 #'
 #' parse_dates(beer_dates_string)
 #'
-sequence_dates <- function(string){
-  date_int <- lubridate::interval(string)
-  date_start <- lubridate::as_date(lubridate::int_start(date_int))
-  date_end <- lubridate::as_date(lubridate::int_end(date_int))
-  sequence_interval <- seq(date_start,date_end,by = "day")
-  return(sequence_interval)
-}
 
-transform_day_numbers <- function(name_day){
-  as.integer(factor(name_day, levels = c("sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"), ordered = TRUE))
-}
+parse_dates <- function(x) {
+  sequence_dates <- function(string){
+    date_int <- lubridate::interval(string)
+    date_start <- lubridate::as_date(lubridate::int_start(date_int))
+    date_end <- lubridate::as_date(lubridate::int_end(date_int))
+    sequence_interval <- seq(date_start,date_end,by = "day")
+    return(sequence_interval)
+  }
 
-transform_day_of_week <- function(name){
-  today_date <- lubridate::today()
-  today_name <- lubridate::wday(today_date)
-  input_name <- transform_day_numbers(name)
-  diff_days <- ifelse(input_name>=today_name,input_name-today_name, 7-today_name + input_name)
-  date_of_interest <- lubridate::today() + lubridate::days(diff_days)
-  return(date_of_interest)
-}
+  transform_day_numbers <- function(name_day){
+    as.integer(factor(name_day, levels = c("sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"), ordered = TRUE))
+  }
 
-kind_of_dates <- function(obs){
-  new_obs <- c()
-  for(i in 1:length(obs)){
-    if(grepl('/', obs[i])){
-      new_obs <- c(new_obs, sequence_dates(obs[i]))
-    } else if(grepl('day', obs[i])){
+  transform_day_of_week <- function(name){
+    today_date <- lubridate::today()
+    today_name <- lubridate::wday(today_date)
+    input_name <- transform_day_numbers(name)
+    diff_days <- ifelse(input_name>=today_name,input_name-today_name, 7-today_name + input_name)
+    date_of_interest <- lubridate::today() + lubridate::days(diff_days)
+    return(date_of_interest)
+  }
+
+  kind_of_dates <- function(obs){
+    new_obs <- c()
+    for(i in 1:length(obs)){
+      if(grepl('/', obs[i])){
+        new_obs <- c(new_obs, sequence_dates(obs[i]))
+      } else if(grepl('day', obs[i])){
 
         if(grepl("[[:upper:]]",obs[i])){
           warning("Please, write the name of the day using the lowercase")}
         new_obs <- c(new_obs, transform_day_of_week(obs[i]))
 
-    } else {
-      new_obs <- c(new_obs, lubridate::as_date(obs[i]))
+      } else {
+        new_obs <- c(new_obs, lubridate::as_date(obs[i]))
+      }
     }
+    return(new_obs)
   }
-  return(new_obs)
-}
-
-parse_dates <- function(x) {
   lapply(lapply(x, kind_of_dates), lubridate::as_date)
 }
