81bb98655e805c4105f95cdcf1c524934fddd226
Valentina Zangirolami <valentina.zangirolami@gmail.com>
2023-04-30 14:30:50 +0200

    add second point last ex

--- R/parse.R
+++ R/parse.R
@@ -21,11 +21,30 @@ sequence_dates <- function(string){
   return(sequence_interval)
 }
 
+transform_day_numbers <- function(name_day){
+  as.integer(factor(name_day, levels = c("sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"), ordered = TRUE))
+}
+
+transform_day_of_week <- function(name){
+  today_date <- lubridate::today()
+  today_name <- lubridate::wday(today_date)
+  input_name <- transform_day_numbers(name)
+  diff_days <- ifelse(input_name>=today_name,input_name-today_name, 7-today_name + input_name)
+  date_of_interest <- lubridate::today() + lubridate::days(diff_days)
+  return(date_of_interest)
+}
+
 kind_of_dates <- function(obs){
   new_obs <- c()
   for(i in 1:length(obs)){
     if(grepl('/', obs[i])){
       new_obs <- c(new_obs, sequence_dates(obs[i]))
+    } else if(grepl('day', obs[i])){
+
+        if(grepl("[[:upper:]]",obs[i])){
+          warning("Please, write the name of the day using the lowercase")}
+        new_obs <- c(new_obs, transform_day_of_week(obs[i]))
+
     } else {
       new_obs <- c(new_obs, lubridate::as_date(obs[i]))
     }
